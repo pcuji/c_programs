eda5d67c7a21a11c99c67d7dd8b4fcf4aabad517
pcuji <pcuji@hawk.iit.edu>
2021-10-02 16:24:39 -0700

    Add files via upload

---
+++ pointers_2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  pointers_2
+//
+//  Created by Pedro Jose Cuji on 1/27/17.
+//  Copyright © 2017 Pedro Jose Cuji. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    
+    printf("Hello, World!\n");
+    return 0;
+}
